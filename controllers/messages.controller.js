1d9ad3b51294b3c988b7581d0a331dc403fca264
Sean <seanzzz@Seans-Mac-Studio.local>
2023-04-27 11:16:33 -0600

    SSR route rendering

--- controllers/messages.controller.js
+++ controllers/messages.controller.js
@@ -1,5 +1,15 @@
+const path = require("path");
+// /folder/files.jpg \folder\files.jpg
+
 function getMessages(req, res) {
-  res.send("<ul><li>Hello</li></ul>");
+  res.render("messages", {
+    title: "Messages to my friends!",
+    friend: "New Friend",
+  });
+  // res.sendFile(
+  //   path.join(__dirname, "..", "public", "images", "skimountain.jpg")
+  // );
+  // res.send("<ul><li>Hello</li></ul>");
 }
 
 function postMessages(req, res) {
--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const path = require("path");
 
 const friendsRouter = require("./routes/friends.router");
 const messageRouter = require("./routes/messages.route");
 
 const app = express();
+
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "views"));
 
 const PORT = 3000;
 
@@ -12,11 +16,18 @@ app.use((req, res, next) => {
   next();
   // actions go here ...
   const delta = Date.now() - start;
-  console.log(`${req.method} ${req.url} ${delta} ms`);
+  console.log(`${req.method} ${req.baseUrl}${req.url} ${delta} ms`);
 });
 
+app.use("/site", express.static(path.join(__dirname, "public")));
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.render("index", {
+    title: "New Test title",
+    caption: "Let's go skiing!",
+  });
+});
 app.use("/friends", friendsRouter);
 app.use("/messages", messageRouter);
 
